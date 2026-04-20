eadc9046f657f5cea14f04262e51aff3bdbb8ce0
hussain-hamza23 <hzhussain23@gmail.com>
2026-04-20 18:41:01 -0400

    added security checks

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,21 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-
-// export default defineConfig([
-//   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-//   tseslint.configs.recommended,
-// ]);
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
-  {
-    // Ignore the compiled output
-    ignores: ["dist/**"],
-  },
+    {
+        ignores: ["dist/**", "node_modules/**"],
+    },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    // Change browser to node here
-    languageOptions: { globals: globals.node } 
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
